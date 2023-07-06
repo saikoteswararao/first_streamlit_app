59215bac26ef2f6ebad0611056b1ea23a65a3b50
saikoteswararao <33627712+saikoteswararao@users.noreply.github.com>
2023-07-06 13:07:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,18 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+#Allow end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+    return "Thanks for adding " + new_fruit
+  
+add_my_fruit = streamlit.text_input("What fruit do you like to add?")
+if streamlit.button("Add a fruit to the List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)  
   
 streamlit.stop()
 
@@ -66,8 +78,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-#Allow end user to add a fruit to the list
-add_my_fruit = streamlit.text_input("What fruit do you like to add?")
+
 
 streamlit.write("Thanks for adding ", add_my_fruit)
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
